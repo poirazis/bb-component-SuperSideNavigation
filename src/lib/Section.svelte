d5e5336d26813a35488495299b687ca781c1cd78
poirazis <poirazis@gmail.com>
2022-12-23 13:57:41 +0200

    Make Sections Collapsible

---
+++ src/lib/Section.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let collapsible = false
+    export let collapsed = false
+    export let value
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<li class="spectrum-SideNav-item">
+    <h2 on:click={() => collapsed = collapsible ? !collapsed : false } class:collapsible={collapsible} class="spectrum-SideNav-heading">{value}</h2>
+    {#if $$slots.default && !collapsed}
+        <ul class="spectrum-SideNav" aria-labelledby="nav-heading-section-{value}">
+            <slot />
+        </ul>
+    {/if}
+</li>
+
+<style>
+    .collapsible:hover {
+        cursor: pointer;
+        color: var(--primaryColor);
+    }
+</style>
